8115c045f212f719a4c07576e1cffc4f8c8fac54
hxlloworld <michael.xia9@gmail.com>
2025-03-07 00:35:35 -0500

    -m

--- .config/fish/functions/commitdots.fish
+++ .config/fish/functions/commitdots.fish
@@ -2,6 +2,6 @@ function commitdots --argument-names message
     cd ~/.config
     yadm add alacritty arkrc btop controls.txt dconf dunst Electron fastfetch fish fontconfig glib-2.0 go gtk-3.0 gtk-4.0 hypr kate kate-externaltoolspluginrc katerc katevirc KDE kde.org kitty Kvantum kwriterc nwg-look pulse qt6ct spicetify QtProject.conf starship.toml systemd Thunar wal waybar waypaper wleave wofi wpg xfce4 xsettingsd
     cd ~
-    yadm add .bashrc .themes .icons Wallpapers
+    yadm add .bashrc .themes/Gruvbox-Orange-Dark .icons/Gruvbox-Light Wallpapers
     yadm commit -m $message
 end
